91f7916bda2b2b1d32dc5806631d48fa6b8bcf2e
kiran9k <kiran9k99@gmail.com>
2013-06-14 10:00:46 +0530

    Update assignment1.py
    added few comment lines

--- assignment1.py
+++ assignment1.py
@@ -4,8 +4,15 @@ print "using linear search"
 x=0
 while (a[x]<>1):
     x=x+1
-print x-1
-print"using another method:"
+print x-1  # it gives the position of 1st array
+################################################
+
+""" the second method is much more effective than the first
+It first searches the elements by raising the array index to power of 2 ,. i.e, indexed search. 
+then it uses the binary search method . """
+
+#######################################
+print"using another method:" 
 while(a[2**i]!=1):
     i=i+1
     print i
